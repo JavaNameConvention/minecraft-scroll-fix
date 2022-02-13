e68c1ad592104193cd6017f1c598ba594240cc70
Yiken <77466195+JavaNameConvention@users.noreply.github.com>
2022-02-13 11:27:23 -0800

    Update macos-fix-scroll.sh

--- macos-fix-scroll.sh
+++ macos-fix-scroll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L 'https://github.com/GameParrot/minecraft-mac-window-fix/blob/main/mac-runtime-patch.zip?raw=true' > /tmp/mac-runtime-patch.zip
+curl -L 'https://github.com/JavaNameConvention/minecraft-scroll-fix/blob/main/mac-runtime-patch.zip?raw=true' > /tmp/mac-runtime-patch.zip
 unzip -o /tmp/mac-runtime-patch.zip -d ~/Library/Application\ Support/minecraft/
 rm /tmp/mac-runtime-patch.zip
 echo 'Successfully installed. To complete installation, set the Java executable to "'"$HOME"'/Library/Application Support/minecraft/mac-runtime-patch/java-runtime-beta/mclaunch" for Java 17, "'"$HOME"'/Library/Application Support/minecraft/mac-runtime-patch/java-runtime-alpha/mclaunch" for Java 16, or "'"$HOME"'/Library/Application Support/minecraft/mac-runtime-patch/jre-legacy/mclaunch" for Java 8.'
